bfd2cbb85560ea2f3e612e1bad68f4d43af9326f
Calgaryman20 <142475032+Calgaryman20@users.noreply.github.com>
2025-11-09 03:47:05 -0500

    Add comments for function clarity in functions.py
    Added comments to clarify the purpose of functions.

--- functions.py
+++ functions.py
@@ -1,12 +1,14 @@
 import psycopg2
 import datetime
 
+# Information needed to connect to the database
 login = {
     "dbname": "assignment3",
     "user": "postgres",
     "password": "password"
 }
 
+# Connect to the Database
 def connect():
     conn = psycopg2.connect(**login)
     if conn.closed == 0:
@@ -15,7 +17,7 @@ def connect():
         print("CONNECTION ERROR")
         exit(1)
 
-
+# Prints all rows of the database
 def getAllStudents():
     conn = connect()
     cur = conn.cursor()
@@ -27,6 +29,7 @@ def getAllStudents():
     cur.close()
     conn.close()
 
+# Add a Row to the database
 def addStudent(first_name, last_name, email, enrollment_date):
     conn = connect()    
     cur = conn.cursor()
@@ -36,6 +39,7 @@ def addStudent(first_name, last_name, email, enrollment_date):
     cur.close()
     conn.close()
 
+# Alters designated rows email attribute
 def updateStudentEmail(student_id, new_email):
     conn = connect()    
     cur = conn.cursor()
@@ -45,6 +49,7 @@ def updateStudentEmail(student_id, new_email):
     cur.close()
     conn.close()
 
+# Removes designated row from table 
 def deleteStudent(student_id):
     conn = connect()    
     cur = conn.cursor()
